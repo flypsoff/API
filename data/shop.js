bba50935dec7f26c499679eaf0a5aa72927ec948
Vlad Sevostianov <flypsoff@gmail.com>
2020-12-24 11:35:54 +0200

    "store"

---
+++ data/shop.js
@@ -0,0 +1,27 @@
+const api = {
+    brands: [
+        {name: 'BMW', id: 1}, 
+        {name: 'Mercedes-Benz', id: 2}, 
+        {name: 'Volkswagen', id: 3}, 
+        {name: 'Audi', id: 4}
+    ],
+    cars: [
+        { carID: 131, brand: 'bmw', model: '740', class: 'S', year: 2019, price: 110555 },
+        { carID: 140, brand: 'volkswagen', model: 'Passat B6', class: 'C', year: 2005, price: 8720 },
+        { carID: 132, brand: 'mercedes-benz', model: '200', class: 'C', year: 2010, price: 10500 },
+        { carID: 133, brand: 'audi', model: 'a4', class: 'C', year: 2012, price: 11750 },
+        { carID: 144, brand: 'volkswagen', model: 'CC', class: 'E', year: 2009, price: 10000 },
+        { carID: 134, brand: 'mercedes-benz', model: '220', class: 'E', year: 2015, price: 17000 },
+        { carID: 135, brand: 'audi', model: 'a8', class: 'S', year: 2020, price: 77950 },
+        { carID: 141, brand: 'volkswagen', model: 'arteon', class: 'S', year: 2020, price: 50750 },
+        { carID: 136, brand: 'mercedes-benz', model: '250', class: 'CLA', year: 2014, price: 21700 },
+        { carID: 137, brand: 'bmw', model: '318', class: 'C', year: 2012, price: 13400 },
+        { carID: 143, brand: 'volkswagen', model: 'Golf', class: 'C', year: 2015, price: 17500 },
+        { carID: 138, brand: 'audi', model: 'a8', class: 'S', year: 2008, price: 14050 },
+        { carID: 139, brand: 'bmw', model: '530', class: 'E', year: 2018, price: 35100 },
+        { carID: 142, brand: 'mercedes-benz', model: '300', class: 'C', year: 2015, price: 15750 },
+
+    ]
+}
+
+module.exports = api 
